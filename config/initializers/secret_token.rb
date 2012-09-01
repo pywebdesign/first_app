f319c97d112c099dd1eb979afa547d5b258e9783
PyWeb <pierre-yves@pywebdesign.net>
2012-09-01 15:01:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6daac083cdc7e335797054f7152bc0b9ae99a92d76b3f76491247e03b5e7a8a459125150acf614f0eb10ffafca244d35d6f8f38e6b2afabe4ecb47f23a242a20'
